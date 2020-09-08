19f17c352f8e6cf3628b908abd427856ce05756f
Benjamin Morgan-Cleveland <benmorgancleveland@gmail.com>
2020-09-08 16:06:46 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -39,7 +39,7 @@ def turn(board)
   if winner(board)
     puts "Congratulations (winner(board))"
   end
-    elsif puts "Please enter 1-9:"
+   puts "Please enter 1-9:"
   input = gets.strip
   index = input_to_index(input)
   if valid_move?(board, index)
