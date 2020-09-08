e183bec8ca14e5a9dbb69eed7e3537b9fb113ac5
Benjamin Morgan-Cleveland <benmorgancleveland@gmail.com>
2020-09-08 15:45:36 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -38,7 +38,6 @@ def turn(board)
   index = input_to_index(input)
   if valid_move?(board, index)
     turn_count(board)
-    current_player(board)
     move(board, index, current_player(board))
     display_board(board)
   else
